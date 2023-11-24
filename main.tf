56d890f3ab4e44c8a41c7ae64a8cc61f441a5a09
AKhilesh <kothapallyakhil123@gmail.com>
2023-11-24 15:10:21 +0530

    updated code

--- main.tf
+++ main.tf
@@ -70,10 +70,10 @@ resource "aws_autoscaling_group" "main" {
 
 resource "aws_route53_record" "main" {
   zone_id = var.zone_id
-  name    = "${var.component}-${var.env}"
+  name    = var.component == "frontend" ? var.env : "${var.component}-${var.env}"
   type    = "CNAME"
   ttl     = 30
-  records = var.alb_name
+  records = [var.private_alb_name]
 }
 
 resource "aws_lb_target_group" "main" {
@@ -86,7 +86,7 @@ resource "aws_lb_target_group" "main" {
 
 
 resource "aws_lb_listener_rule" "main" {
-  listener_arn = var.listener
+  listener_arn = var.private_listener
   priority     = var.lb_priority
 
   action {
