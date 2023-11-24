e26a9c32efdabc0790c244125e2baff16434f912
AKhilesh <kothapallyakhil123@gmail.com>
2023-11-24 13:09:55 +0530

    updated code

--- main.tf
+++ main.tf
@@ -66,3 +66,37 @@ resource "aws_autoscaling_group" "main" {
   }
 
 }
+
+resource "aws_route53_record" "main" {
+  zone_id = var.zone_id
+  name    = "${var.component}-${var.env}"
+  type    = "CNAME"
+  ttl     = 30
+  records = var.alb_name
+}
+
+resource "aws_lb_target_group" "main" {
+  name     = local.name_prefix
+  port     = var.port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+
+
+resource "aws_lb_listener_rule" "main" {
+  listener_arn = var.listener
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.component}-${var.env}.akhildevops.online"]
+    }
+  }
+
+}
