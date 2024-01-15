d150465158d5c5cb8aef09f17fcf19cebab623b1
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-15 12:44:29 +0530

    updated code

--- vars.tf
+++ vars.tf
@@ -7,16 +7,3 @@ variable "port" {}
 variable "sg_ingress_cidr" {}
 variable "ssh_ingress_cidr" {}
 variable "zone_id" {}
-variable "instance_type" {}
-variable "desired_capacity" {}
-variable "max_size" {}
-variable "min_size" {}
-variable "private_alb_name" {}
-variable "private_listener" {}
-variable "lb_priority" {}
-variable "public_alb_name" {}
-variable "public_listener" {}
-variable "default_vpc_id" {}
-variable "parameters" {}
-variable "kms_key_id" {}
-variable "monitoring_ingress_cidr" {}
