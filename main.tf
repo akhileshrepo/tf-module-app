63103b03d6feba9ae0fc47ee0bcc426bf108f994
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-17 17:06:51 +0530

    updated code

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "aws_autoscaling_group" "bar" {
   desired_capacity    = var.desired_capacity
   max_size            = var.max_size
   min_size            = var.min_size
-  target_group_arns = [aws_lb_target_group.main.arn]
+  target_group_arns   = [aws_lb_target_group.main.arn]
 
   launch_template {
     id      = aws_launch_template.main.id
