375da7e83773501ee76e222eec38309a05591247
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-24 13:24:21 +0530

    updated code

--- main.tf
+++ main.tf
@@ -35,10 +35,10 @@ resource "aws_launch_template" "main" {
   image_id      = data.aws_ami.ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
-  user_data = base64encode(templatefile("${path.module}/userdata.sh"),
+  user_data = base64encode(templatefile("${path.module}/userdata.sh",
     {
       component = var.component
-    })
+    }))
 
   tag_specifications {
     resource_type = "instance"
