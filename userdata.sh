d4bfa1dfa0d658dd28c5a55409ede989dfb807d1
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-26 13:53:42 +0530

    updated code

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install ansible -y &>>/opt/userdata.log
-ansible-pull -i localhost, -U https://github.com/akhileshrepo/roboshop-ansible.git main.yml -e component=${component} &>>/opt/userdata.log
+yum install ansible python3.11-pip.noarch -y &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/akhileshrepo/roboshop-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
 
 
