23d40808cf469215c663700a828717ac220895f8
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-26 12:25:03 +0530

    updated code

--- main.tf
+++ main.tf
@@ -97,12 +97,17 @@ resource "aws_lb_listener_rule" "main" {
   }
 }
 
-/*
 resource "aws_lb_target_group" "public" {
   count    = var.component == "frontend" ? 1 : 0
-  name     = local.name_prefix
+  name     = "${local.name_prefix}-public"
   port     = var.port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 }
-*/
+
+resource "aws_lb_target_group_attachment" "public" {
+  count               = length(tolist(data.dns_a_record_set.private_alb.addrs))
+  target_group_arn    = aws_lb_target_group.public[0].arn
+  target_id           = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index)
+  port                = 80
+}
