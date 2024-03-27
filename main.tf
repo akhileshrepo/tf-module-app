1f32e25d6694b7f0d12a1feb54caaeeb3e786bf4
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-27 11:44:34 +0530

    updated code

--- main.tf
+++ main.tf
@@ -42,7 +42,6 @@ resource "aws_iam_policy" "main" {
         "Sid" : "VisualEditor0",
         "Effect" : "Allow",
         "Action" : [
-          "kms:Decrypt",
           "ssm:GetParameterHistory",
           "ssm:GetParametersByPath",
           "ssm:GetParameters",
