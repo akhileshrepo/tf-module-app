5a4efcc12a4d523e7f9acd03239463caae42d823
AKhilesh <kothapallyakhil123@gmail.com>
2024-04-08 12:40:02 +0530

    updated code

--- main.tf
+++ main.tf
@@ -38,13 +38,13 @@ resource "aws_security_group" "main" {
 }
 
 resource "aws_security_group_rule" "nginx_exporter" {
-  count = var.component ==  "frontend" ? 1 : 0
+  count             = var.component == "frontend" ? 1 : 0
   type              = "ingress"
   from_port         = 9113
   to_port           = 9113
   protocol          = "tcp"
   cidr_blocks       = var.monitoring_ingress_cidr
-  ipv6_cidr_blocks  = aws_security_group.main.id
+  security_group_id = aws_security_group.main.id
   description       = "Nginx Prometheus Exporter"
 }
 
