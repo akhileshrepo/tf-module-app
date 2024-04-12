e90d69c52456a6c7a879ba9ba938d44cc4ed6649
AKhilesh <kothapallyakhil123@gmail.com>
2024-04-12 14:07:32 +0530

    updated code

--- main.tf
+++ main.tf
@@ -208,14 +208,25 @@ resource "aws_lb_target_group" "public" {
   target_type = "ip"
   protocol    = "HTTP"
   vpc_id      = var.default_vpc_id
+
+  health_check {
+    enabled = true
+    healthy_threshold = 2
+    interval  = 5
+    path = "/"
+    port = var.port
+    timeout = 2
+    unhealthy_threshold = 2
+  }
 }
 
 resource "aws_lb_target_group_attachment" "public" {
-  count             = var.component == "frontend" ? length(var.az) : 0
-  target_group_arn  = aws_lb_target_group.public[0].arn
-  target_id         = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index)
-  port              = 80
-  availability_zone = "all"
+  count              = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb.addrs)) : 0
+  #count             = var.component == "frontend" ? length(var.az) : 0
+  target_group_arn   = aws_lb_target_group.public[0].arn
+  target_id          = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index)
+  port               = 80
+  availability_zone  = "all"
 }
 
 resource "aws_lb_listener_rule" "public" {
