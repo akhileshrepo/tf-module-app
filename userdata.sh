b7fa4fc2381a2eda315079aa106325153518cb7d
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-01 02:33:39 +0530

    updated code

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum install ansible -y
+labauto ansible
 ansible-pull -i localhost, -U https://github.com/akhileshrepo/roboshop-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
 
 
