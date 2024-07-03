1901bfc8b46c9585050cf5f969df68b713863b96
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-03 14:10:30 +0530

    updated code

--- vars.tf
+++ vars.tf
@@ -11,6 +11,8 @@ variable "subnet_ids" {}
 variable "desired_capacity" {}
 variable "max_size" {}
 variable "min_size" {}
-variable "alb_name" {}
-variable "listener" {}
+variable "private_alb_name" {}
+variable "private_listener" {}
 variable "lb_priority" {}
+variable "public_alb_name" {}
+variable "public_listener" {}
