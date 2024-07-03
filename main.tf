781e57a9f5c74cd771a22864c109617124ce074a
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-03 11:43:02 +0530

    updated code

--- main.tf
+++ main.tf
@@ -52,6 +52,7 @@ resource "aws_autoscaling_group" "main" {
   desired_capacity        = var.desired_capacity
   max_size                = var.max_size
   min_size                = var.min_size
+  target_group_arns       = [aws_lb_target_group.main.arn]
 
   launch_template {
     id = aws_launch_template.main.id
