b1b79ad030936d4467ff4a661a4197b1eaae7443
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-03 14:11:47 +0530

    updated code

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ resource "aws_route53_record" "main" {
   name    = var.component == "frontend" ? var.env : "${var.component}-${var.env}"
   type    = "CNAME"
   ttl     = 30
-  records = [var.private_alb_name]
+  records = [var.component == "frontend" ? var.public_alb_name : var.private_alb_name ]
 }
 
 resource "aws_lb_target_group" "main" {
