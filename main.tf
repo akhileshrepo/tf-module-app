e99baceec5b1b5b88e1eecb52393766cd1b49525
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-03 16:49:12 +0530

    updated code

--- main.tf
+++ main.tf
@@ -107,7 +107,7 @@ resource "aws_lb_target_group" "public" {
 resource "aws_lb_target_group_attachment" "test" {
   count = length(data.dns_a_record_set.private_alb.addrs)
   target_group_arn = aws_lb_target_group.public[0].arn
-  target_id        = element(data.dns_a_record_set.private_alb.addrs, count.index)
+  target_id        = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index)
   port             = 80
 }
 
