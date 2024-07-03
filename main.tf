48545a1d4d402e3f3f9e3156e6fdc3d489e3a55a
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-03 17:38:01 +0530

    updated code

--- main.tf
+++ main.tf
@@ -106,7 +106,7 @@ resource "aws_lb_target_group" "public" {
 }
 
 resource "aws_lb_target_group_attachment" "public" {
-  count = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb.addrs)) : 0
+  count = var.component == "frontend" ? length(var.az) : 0
   target_group_arn = aws_lb_target_group.public[0].arn
   target_id        = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index)
   port             = 80
