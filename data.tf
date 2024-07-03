1e4fb6dff1e8fb3c15d82d9529eb916739bdf8fc
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-03 10:37:04 +0530

    updated code

---
+++ data.tf
@@ -0,0 +1,5 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
--- vars.tf
+++ vars.tf
@@ -6,3 +6,8 @@ variable "port" {}
 variable "sg_ingress_cidr" {}
 variable "ssh_ingress_cidr" {}
 variable "zone_id" {}
+variable "instance_type" {}
+variable "subnet_ids" {}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
