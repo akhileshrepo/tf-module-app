7ec2135e5f34db75817c09ef00d9b630e0c84a7f
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-04 00:25:55 +0530

    updated code

--- main.tf
+++ main.tf
@@ -38,6 +38,7 @@ resource "aws_launch_template" "main" {
   user_data = base64encode(templatefile("${path.module}/userdata.sh",
     {
       component = var.component
+      env       = var.env
     }))
 
   tag_specifications {
