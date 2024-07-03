d89b9db2537e11f20ee045fac61220fde8bd5d48
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-03 14:31:07 +0530

    updated code

--- main.tf
+++ main.tf
@@ -95,3 +95,11 @@ resource "aws_lb_listener_rule" "main" {
     }
   }
 }
+
+resource "aws_lb_target_group" "public" {
+  count = var.component == "frontend" ? 1 : 0
+  name = "${local.name_prefix}-public"
+  port = var.port
+  protocol = "HTTP"
+  vpc_id = var.vpc_id
+}
