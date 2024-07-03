42bee633f2581664072aedd16c0b27ddd2850d28
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-04 00:51:49 +0530

    updated code

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-yum install ansible -y &>>/opt/userdata.log
-pip3.11 install botocore boto3 &>>/opt/userdata.log
+yum install python3.12-pip.noarch -y &>>/opt/userdata.log
+pip3.12 install botocore boto3 &>>/opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/akhileshrepo/roboshop-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
