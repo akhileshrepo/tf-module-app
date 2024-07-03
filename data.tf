a5aca1214db015644c98780edb265b9b68269614
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-03 16:45:52 +0530

    updated code

--- data.tf
+++ data.tf
@@ -3,3 +3,7 @@ data "aws_ami" "ami" {
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
 }
+
+data "dns_a_record_set" "private_alb" {
+  host = var.private_alb_name
+}
--- main.tf
+++ main.tf
@@ -103,3 +103,11 @@ resource "aws_lb_target_group" "public" {
   protocol = "HTTP"
   vpc_id = var.vpc_id
 }
+
+resource "aws_lb_target_group_attachment" "test" {
+  count = length(data.dns_a_record_set.private_alb.addrs)
+  target_group_arn = aws_lb_target_group.public[0].arn
+  target_id        = element(data.dns_a_record_set.private_alb.addrs, count.index)
+  port             = 80
+}
+
