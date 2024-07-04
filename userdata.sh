7b6ba2c152a9f6cdd70b5d7271335be3c3b09d33
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-04 15:25:16 +0530

    updated code

--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-labauto ansible
+
 ansible-pull -i localhost, -U https://github.com/akhileshrepo/roboshop-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
