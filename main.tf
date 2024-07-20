9348e59926ed0e73cfc02e5b6531694e8d3d0e76
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-20 18:19:07 +0530

    updated code

--- main.tf
+++ main.tf
@@ -168,6 +168,16 @@ resource "aws_lb_target_group" "main" {
   port     = var.port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 5
+    path                = "/health"
+    port                = var.port
+    timeout             = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
 }
 
 resource "aws_lb_listener_rule" "main" {
@@ -204,7 +214,6 @@ resource "aws_lb_target_group" "public" {
     unhealthy_threshold = 2
     matcher             = "404"
   }
-
 }
 
 resource "aws_lb_target_group_attachment" "public" {
