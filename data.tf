1a7f63501a5436e18d5bc492eff3852877823d3b
AKhilesh <kothapallyakhil123@gmail.com>
2024-08-27 22:05:59 +0530

    changes

--- data.tf
+++ data.tf
@@ -1,7 +1,13 @@
+# data "aws_ami" "ami" {
+#   most_recent = true
+#   name_regex  = "Centos-8-DevOps-Practice"
+#   owners      = ["973714476881"]
+# }
+
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
+  name_regex  = "roboshop-ami-v1"
+  owners      = ["058264419835"]
 }
 
 data "dns_a_record_set" "private_alb" {
--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-dnf install python3.12-pip ansible -y | tee -a /opt/userdata.log
-pip3.12 install boto3 botocore | tee -a /opt/userdata.log
+#dnf install python3.12-pip ansible -y | tee -a /opt/userdata.log
+#pip3.12 install boto3 botocore | tee -a /opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/akhileshrepo/roboshop-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
