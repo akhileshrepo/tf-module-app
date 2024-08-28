078ee2d7b6040a1a2a9dc0d6d70b54bb7c7af18f
AKhilesh <kothapallyakhil123@gmail.com>
2024-08-28 12:45:06 +0530

    changes

--- main.tf
+++ main.tf
@@ -168,6 +168,20 @@ resource "aws_autoscaling_group" "main" {
   }
 }
 
+resource "aws_autoscaling_policy" "asg-cpu-rule" {
+  name                      = "CPULoadDetect"
+  autoscaling_group_name    = aws_autoscaling_group.main.name
+  policy_type               = "TargetTrackingScaling"
+  estimated_instance_warmup = 120
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 50.0
+  }
+}
+
+
 resource "aws_route53_record" "main" {
   zone_id = var.zone_id
   name    = var.component == "frontend" ? var.env == "prod" ? "www" : var.env : "${var.component}-${var.env}"
